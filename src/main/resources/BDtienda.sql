2a4fc4012a5311471cd041058e93531c58ae32cf
Arturo96 <carlosospino13@gmail.com>
2020-03-12 16:10:12 -0500

    Diseño logico (BD) del login

---
+++ src/main/resources/BDtienda.sql
@@ -0,0 +1,143 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Linux (x86_64)
+--
+-- Host: localhost    Database: tienda
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `CLIENTE`
+--
+
+DROP TABLE IF EXISTS `CLIENTE`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `CLIENTE` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `documento` varchar(15) NOT NULL,
+  `nombres` varchar(100) NOT NULL,
+  `apellidos` varchar(70) DEFAULT NULL,
+  `telefono` varchar(15) NOT NULL,
+  `direccion` varchar(100) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `documento_UNIQUE` (`documento`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `CLIENTE`
+--
+
+LOCK TABLES `CLIENTE` WRITE;
+/*!40000 ALTER TABLE `CLIENTE` DISABLE KEYS */;
+INSERT INTO `CLIENTE` VALUES (1,'1145246812','Daniel','Tellez Vargas','3014579457','Cra 35 # 10 - 45','lagarraasecina@hotmail.com');
+/*!40000 ALTER TABLE `CLIENTE` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `EMPLEADO`
+--
+
+DROP TABLE IF EXISTS `EMPLEADO`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `EMPLEADO` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombres` varchar(100) NOT NULL,
+  `apellidos` varchar(70) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `EMPLEADO`
+--
+
+LOCK TABLES `EMPLEADO` WRITE;
+/*!40000 ALTER TABLE `EMPLEADO` DISABLE KEYS */;
+INSERT INTO `EMPLEADO` VALUES (1,'Carlos Arturo','Vargas'),(2,'Manuel','Villegas');
+/*!40000 ALTER TABLE `EMPLEADO` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ROL`
+--
+
+DROP TABLE IF EXISTS `ROL`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ROL` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nombre_UNIQUE` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ROL`
+--
+
+LOCK TABLES `ROL` WRITE;
+/*!40000 ALTER TABLE `ROL` DISABLE KEYS */;
+INSERT INTO `ROL` VALUES (1,'Administrador'),(3,'Auditor'),(4,'Cliente'),(2,'Vendedor');
+/*!40000 ALTER TABLE `ROL` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `USUARIO`
+--
+
+DROP TABLE IF EXISTS `USUARIO`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `USUARIO` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(40) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `rol` int NOT NULL,
+  `cliente` int DEFAULT NULL,
+  `empleado` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `cliente_UNIQUE` (`cliente`),
+  UNIQUE KEY `empleado_UNIQUE` (`empleado`),
+  KEY `FK_USUARIO_ROL_idx` (`rol`),
+  CONSTRAINT `FK_USUARIO_CLI` FOREIGN KEY (`cliente`) REFERENCES `CLIENTE` (`id`),
+  CONSTRAINT `FK_USUARIO_EMP` FOREIGN KEY (`empleado`) REFERENCES `EMPLEADO` (`id`),
+  CONSTRAINT `FK_USUARIO_ROL` FOREIGN KEY (`rol`) REFERENCES `ROL` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `USUARIO`
+--
+
+LOCK TABLES `USUARIO` WRITE;
+/*!40000 ALTER TABLE `USUARIO` DISABLE KEYS */;
+INSERT INTO `USUARIO` VALUES (1,'pacho','pacho',4,1,NULL),(2,'arturo96','admin',1,NULL,1),(3,'tandu','tandu',2,NULL,2);
+/*!40000 ALTER TABLE `USUARIO` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-03-12 16:06:13
